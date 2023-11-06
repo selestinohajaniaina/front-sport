a5f81c1cfe4ad9a47a0464d6b982e6a55d23842b
Zo Nirina <tomzonirina@gmail.com>
2023-11-06 18:35:58 +0300

    feat: first push

---
+++ FRONT_SPORT/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-amber-400 w-full h-[100dvh] bg-black/80 text-center'>
+        Nice
+      </div>
+    </>
+  )
+}
+
+export default App
